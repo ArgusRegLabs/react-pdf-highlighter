8eb4ed06877d9b0eac83ee63a714100686056fe3
Rehan Rehman <49568477+rehanpunjwani@users.noreply.github.com>
2024-10-03 14:00:19 -0500

    fix: build

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,13 +5,22 @@ export default defineConfig({
   base: "/react-pdf-highlighter/",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      external: [
+        "react",
+        "react/jsx-runtime",
+        "react-dom",
+        "react-dom/client",
+        "react-rnd",
+        "pdfjs-dist",
+        "pdfjs-dist/web",
+        "debounce",
+      ],
+    },
   },
   plugins: [react()],
   server: {
     port: 3003,
-  },
-  optimizeDeps: {
-    include: ["react/jsx-runtime"],
   },
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
